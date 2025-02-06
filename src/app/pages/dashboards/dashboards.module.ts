8e84cbb2a1459264bb0f57adca76e8e8d4dda22b
AxelDatum <axel.hernandez@datumredsoft.com>
2025-02-06 15:24:39 -0600

    feat: refactor dashboards module and replace dashboard components

--- src/app/pages/dashboards/dashboards.module.ts
+++ src/app/pages/dashboards/dashboards.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 
 import { DashboardsRoutes } from './dashboards.routes';
 
-import { AppDashboard1Component } from './dashboard1/dashboard1.component';
+import { AppDashboard1Component } from './dasboard-interno/dashboard-interno.component';
 import { AppDashboard2Component } from './dashboard2/dashboard2.component';
 import { AppDashboardInternoComponent } from './dashboard_interno/dashboard-interno.component';
 import { AppDashboardExternoComponent } from './dashboard_externo/dashboard-externo.component';
--- src/app/pages/dashboards/dashboards.routes.ts
+++ src/app/pages/dashboards/dashboards.routes.ts
@@ -2,46 +2,23 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from 'src/app/guards/auth.guard';
 
 // dashboards
-import { AppDashboard1Component } from './dashboard1/dashboard1.component';
+import { AppDashboardInternoComponent } from './dasboard-interno/dashboard-interno.component';
 import { AppDashboard2Component } from './dashboard2/dashboard2.component';
-import { AppDashboardInternoComponent} from './dashboard_interno/dashboard-interno.component';
 import { AppDashboardExternoComponent } from './dashboard_externo/dashboard-externo.component';
 import { AppSolicitudAfpaComponent } from './solicitud-nuevo-afpa/solicitud-afpa.component';
 import { AppSolicitudBaseComponent } from './solicitud-base/solicitud-base.component';
-import { AppSolicitudExternoComponent } from './solicitud-externo/solicitud-externo.component';
+
 
 export const DashboardsRoutes: Routes = [
   {
     path: '',
     children: [
       {
-        path: 'dashboard1',
-        component: AppDashboard1Component,
-        data: {
-          title: 'Dashboard',
-          urls: [
-            { title: 'Dashboard', url: '/dashboards/dashboard1' },
-            { title: 'Analytical' },
-          ],
-        },
-      },
-      {
-        path: 'dashboard2',
-        component: AppDashboard2Component,
-        data: {
-          title: 'eCommerce',
-          urls: [
-            { title: 'Dashboard', url: '/dashboards/dashboard2' },
-            { title: 'eCommerce' },
-          ],
-        },
-      },
-      {
         path:'dashboard-interno',
         component: AppDashboardInternoComponent,
-        canActivate: [AuthGuard],
+        
         data: {
-          role: 'INTERNO',
+          
           title: 'Analytical',
           urls: [
             { title: 'Dashboard', url: '/dashboards/dashboard-interno' },
